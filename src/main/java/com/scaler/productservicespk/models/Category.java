d7cdd8872fa17d1f6c16083516076b80bd7db336
Preeti <preeti.kumari241116@gmail.com>
2025-04-14 17:14:56 +0530

    Saving 25th commit

--- src/main/java/com/scaler/productservicespk/models/Category.java
+++ src/main/java/com/scaler/productservicespk/models/Category.java
@@ -1,6 +1,7 @@
 package com.scaler.productservicespk.models;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
@@ -13,6 +14,7 @@ import java.util.List;
 @Entity(name = "categories")
 public class Category extends BaseModel{
     private String name;
+   // @Column(unique = true)
     private String value;
 
 /*    @OneToMany(mappedBy = "category",cascade = CascadeType.REMOVE)
